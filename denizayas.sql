ae64ab7d235837851dea0aad7bea9f1cfbb365f9
Deniz Ayas <70910250+denizayaas@users.noreply.github.com>
2022-04-06 10:33:48 +0300

    Add files via upload

---
+++ denizayas.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 17 Haz 2021, 11:24:46
+-- Sunucu sürümü: 10.4.19-MariaDB
+-- PHP Sürümü: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `denizayas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ayar`
+--
+
+CREATE TABLE `ayar` (
+  `ayar_id` int(11) NOT NULL,
+  `ayar_logo` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_url` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_title` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_description` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_keywords` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_author` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_tel` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_gsm` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_faks` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_mail` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_ilce` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_il` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_adres` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_mesai` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_maps` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_analystic` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_zopim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_facebook` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_twitter` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_instagram` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_skype` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_linkedn` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_google` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_youtube` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtphost` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtpuser` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtppassword` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_smtpport` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `ayar_bakim` enum('0','1') COLLATE utf8_turkish_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `ayar`
+--
+
+INSERT INTO `ayar` (`ayar_id`, `ayar_logo`, `ayar_url`, `ayar_title`, `ayar_description`, `ayar_keywords`, `ayar_author`, `ayar_tel`, `ayar_gsm`, `ayar_faks`, `ayar_mail`, `ayar_ilce`, `ayar_il`, `ayar_adres`, `ayar_mesai`, `ayar_maps`, `ayar_analystic`, `ayar_zopim`, `ayar_facebook`, `ayar_twitter`, `ayar_instagram`, `ayar_skype`, `ayar_linkedn`, `ayar_google`, `ayar_youtube`, `ayar_smtphost`, `ayar_smtpuser`, `ayar_smtppassword`, `ayar_smtpport`, `ayar_bakim`) VALUES
+(0, 'dimg/28502303592594530277s.jpg', '', 'Deniz Ayas', 'Deniz Ayas ', 'blog, php, özgeçmiş', 'Deniz Ayas', '', '', '', '', '', '', '', '10-20', 'ayar_maps_api0', 'ayar_analystic0', 'ayar_zopima0', '', '', 'https://www.instagram.com/denizayaas', '', '', '', '', 'mail.alanadiniz.com', 'mail@alanadiniz.com', 'smtppassword', '587', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `hakkimda`
+--
+
+CREATE TABLE `hakkimda` (
+  `hakkimda_id` int(11) NOT NULL,
+  `hakkimda_unvan` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimda_resim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `hakkimda_icerik` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `hakkimda`
+--
+
+INSERT INTO `hakkimda` (`hakkimda_id`, `hakkimda_unvan`, `hakkimda_resim`, `hakkimda_icerik`) VALUES
+(0, 'Frontend Developer', '', 'Deniz Ayas');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanici`
+--
+
+CREATE TABLE `kullanici` (
+  `kullanici_id` int(11) NOT NULL,
+  `kullanici_resim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_tc` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_mail` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_tel` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_password` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adres` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_il` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ilce` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_unvan` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_yetki` varchar(50) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `kullanici`
+--
+
+INSERT INTO `kullanici` (`kullanici_id`, `kullanici_resim`, `kullanici_tc`, `kullanici_ad`, `kullanici_mail`, `kullanici_tel`, `kullanici_password`, `kullanici_adsoyad`, `kullanici_adres`, `kullanici_il`, `kullanici_ilce`, `kullanici_unvan`, `kullanici_yetki`) VALUES
+(1, '', '11122233344', 'Deniz Ayas', 'admin', '534 720 09 05', '827ccb0eea8a706c4c34a16891f84e7b', 'Deniz', '', 'İzmir', 'Bornova', 'Php Developer', '5');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `ayar`
+--
+ALTER TABLE `ayar`
+  ADD PRIMARY KEY (`ayar_id`);
+
+--
+-- Tablo için indeksler `hakkimda`
+--
+ALTER TABLE `hakkimda`
+  ADD PRIMARY KEY (`hakkimda_id`);
+
+--
+-- Tablo için indeksler `kullanici`
+--
+ALTER TABLE `kullanici`
+  ADD PRIMARY KEY (`kullanici_id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanici`
+--
+ALTER TABLE `kullanici`
+  MODIFY `kullanici_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
